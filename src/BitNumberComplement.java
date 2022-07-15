8600ece6e78df9905fd45654a13cd791a2290c9a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-14 23:51:04 -0700

    added a method to get the reversed answer which is the complement of the bit

--- src/BitNumberComplement.java
+++ src/BitNumberComplement.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+
 public class BitNumberComplement {
+
+    public static int bitNumberComplement(int n){
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(n>0){
+            if((n&1)>0){
+                list.add(0);
+
+            }else{
+                list.add(1);
+            }
+            n >>=1;
+        }
+        int ans = 0;
+        for(int i=0; i<list.size(); i++){
+            ans += list.get(i)*1<<1;
+        }return ans;
+
+    }
     public static void main(String[] args) {
+
 
     }
 }
