bb118f8d6e39235264b0e36e8ccd7150947c2a6e
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-14 23:53:21 -0700

    added comments for better understanding

--- src/BitNumberComplement.java
+++ src/BitNumberComplement.java
@@ -3,20 +3,26 @@ import java.util.ArrayList;
 public class BitNumberComplement {
 
     public static int bitNumberComplement(int n){
-        ArrayList<Integer> list = new ArrayList<>();
 
+        // get the list to add the complement values
+        ArrayList<Integer> list = new ArrayList<>();
+        // until n is greater
         while(n>0){
+            // add their reverses
             if((n&1)>0){
                 list.add(0);
 
             }else{
                 list.add(1);
             }
+            //move the list
             n >>=1;
         }
+
         int ans = 0;
         for(int i=0; i<list.size(); i++){
-            ans += list.get(i)*1<<1;
+            // ans will add the  ans and the element in the list multiply by 2^i(1<<i)
+            ans += list.get(i)*1<<i;
         }return ans;
 
     }
